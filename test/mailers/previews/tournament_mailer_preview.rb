eb199ac0c48551f37dfa47e9481d1f343b5b9758
Jascha Haldemann <jh@oxon.ch>
2019-01-02 22:54:30 +0100

    new_tournament_email

--- test/mailers/previews/tournament_mailer_preview.rb
+++ test/mailers/previews/tournament_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/tournament_mailer
 class TournamentMailerPreview < ActionMailer::Preview
   def new_tournament_email
-    TournamentMailer.with(tournament: Tournament.first).new_tournament_email
+    TournamentMailer.with(tournament: Tournament.first, user: User.first).new_tournament_email
   end
 end
