a57ee5e376e5cc598592fc1753b0adc9d15cbfd5
Jascha Haldemann <jh@oxon.ch>
2019-01-03 01:54:41 +0100

    New Admin btn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:destroy]
+  before_action :set_user, only: [:update, :destroy]
   before_action { @section = 'users' }
 
   # GET /users
   # GET /users.json
   def index
     @users = User.all
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if current_user.admin? and @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { redirect_to users_path, alert: "User couldn't be updated." }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /users/1
@@ -26,6 +40,11 @@ class UsersController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:is_admin, :updated_at)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :players
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  resources :users, only: [:index, :destroy]
+  resources :users, only: [:index, :update, :destroy]
 
   root to: "tournaments#index"
 
