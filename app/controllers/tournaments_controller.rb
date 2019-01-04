baeab0868adff861ef80b35cc37efe206aac2555
Jascha Haldemann <jh@oxon.ch>
2019-01-04 18:37:14 +0100

    some view updates

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -158,7 +158,7 @@ class TournamentsController < ApplicationController
       ct.participants.each do |p|
         # updated player
         player = @tournament.players.find_by(:gamer_tag => p.display_name)
-        player.points += points_repartition_table(p.final_rank)
+        player.points += helpers.points_repartition_table(p.final_rank)
         player.participations += 1
         if player.participations >= 30 and player.tournament_experience < 2 then player.tournament_experience = 2
         elsif player.participations >= 10 and player.tournament_experience < 1 then player.tournament_experience = 1
@@ -203,18 +203,4 @@ class TournamentsController < ApplicationController
       Challonge::API.key = ENV['CHALLONGE_API_KEY']
     end
 
-    def points_repartition_table(rank)
-        if rank == 1 then 300
-        elsif rank == 2 then 250
-        elsif rank == 3 then 200
-        elsif rank == 4 then 150
-        elsif rank <= 6 then 100
-        elsif rank <= 8 then 75
-        elsif rank <= 12 then 50
-        elsif rank <= 16 then 25
-        elsif rank <= 24 then 15
-        elsif rank <= 32 then 10
-        else 5
-        end
-    end
 end
--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,5 +1,22 @@
 module TournamentsHelper
+
   def registration_deadline(date)
     date-16*3600
   end
+
+  def points_repartition_table(rank)
+      if rank == 1 then 300
+      elsif rank == 2 then 250
+      elsif rank == 3 then 200
+      elsif rank == 4 then 150
+      elsif rank <= 6 then 100
+      elsif rank <= 8 then 75
+      elsif rank <= 12 then 50
+      elsif rank <= 16 then 25
+      elsif rank <= 24 then 15
+      elsif rank <= 32 then 10
+      else 5
+      end
+  end
+
 end
