e8e93b5c52d572fd778ba15e74bb592ef2ebfbf9
Jascha Haldemann <jh@oxon.ch>
2019-02-10 13:36:50 +0100

    bugfix update tournament exp only when less than

--- app/models/player.rb
+++ app/models/player.rb
@@ -13,15 +13,15 @@ class Player < ApplicationRecord
 
   def update_tournament_experience
     if self.participations == 0
-      self.tournament_experience = 0 # None
+      self.tournament_experience = 0 if self.tournament_experience < 0 # None
     elsif self.participations < 5
-      self.tournament_experience = 1 # A little
+      self.tournament_experience = 1 if self.tournament_experience < 1 # A little
     elsif self.participations < 10
-      self.tournament_experience = 2 # Some
+      self.tournament_experience = 2 if self.tournament_experience < 2 # Some
     elsif self.participations < 30
-      self.tournament_experience = 3 # A lot
+      self.tournament_experience = 3 if self.tournament_experience < 3 # A lot
     else
-      self.tournament_experience = 4 # Very much
+      self.tournament_experience = 4 if self.tournament_experience < 4 # Very much
     end
     self.save
   end
