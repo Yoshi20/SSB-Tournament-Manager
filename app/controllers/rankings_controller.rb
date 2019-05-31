1541b51a6bcd7b839fab7bc132fff8083a6d369e
Jascha Haldemann <jh@oxon.ch>
2019-05-31 18:28:07 +0200

    some unspecific view updates

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -7,14 +7,17 @@ class RankingsController < ApplicationController
     if params[:filter].nil? or params[:filter] == 'all'
       @players = Player.all.includes(:user).order(points: :desc, participations: :asc, created_at: :asc)
     elsif params[:filter] == 'major'
-      # @players = Player.includes(:user).includes(:results).where.not(results: {major_name: nil}).order(points: :desc, participations: :asc, created_at: :asc)
       @players = Player.includes(:user).includes(:results).where("results.major_name LIKE ?", "%#{ActiveRecord::Base.sanitize_sql_like(params[:major])}%").references(:results).sort_by do |p|
-        p.results_sum(params[:major]) << p.created_at
+        p.results_sum(params[:major]) << -p.created_at.to_i
+      end.reverse
+    elsif params[:filter] == 'seed_points'
+      @players = Player.all.includes(:user).order(points: :desc, participations: :asc, created_at: :asc).sort_by do |p|
+        [p.seed_points, -p.created_at.to_i]
       end.reverse
     elsif helpers.tournament_cities.include?(params[:filter].capitalize)
       city = params[:filter].capitalize
       @players = Player.includes(:user).includes(:results).where(results: {city: city}).sort_by do |p|
-        p.results_sum(city) << p.created_at
+        p.results_sum(city) << -p.created_at.to_i
       end.reverse
     end
     @started_and_finished_tournaments_count_2019 = Tournament.active_2019.where(started: true, finished: true).count
