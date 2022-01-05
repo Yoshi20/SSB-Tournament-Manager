acad26ff4af7443c2c19a89dd71d3361f3ce2185
Jascha Haldemann <jh@oxon.ch>
2022-01-05 16:20:34 +0100

    send contact mail to swisssmash not to germanysmash

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 
   def contact_email
     @body = params[:body]
-    mail(from: params[:email], to: 'admin@germanysmash.de', subject: "Nachricht von #{params[:name]}")
+    mail(from: params[:email], to: 'admin@swisssmash.ch', subject: "Nachricht von #{params[:name]}")
   end
 
 end
