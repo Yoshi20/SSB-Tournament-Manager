1d8d6f5ad4500fe2f50c81d74522e50dec52342d
Jascha Haldemann <jh@oxon.ch>
2022-01-05 17:15:30 +0100

    also count the other gender

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -5,7 +5,7 @@ class StatisticsController < ApplicationController
   # GET /statistics.json
   def index
     @players = Player.all
-    @player_count_total = @players.count
+    @player_count_total = @players.count - 4 # do not count bye0, bye1 bye2 & bye3
     @player_count_2019 = @players.from_2019.count
     @player_count_2020 = @players.from_2020.count
     @player_count_2021 = @players.from_2021.count
