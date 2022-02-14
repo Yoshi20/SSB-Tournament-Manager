008567332f3353f3503bb8634f92be939467698b
Jascha Haldemann <jh@oxon.ch>
2022-02-14 17:49:10 +0100

    ActionController::Parameters.new(JSON.parse(params[:data])).permit(...)

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -31,10 +31,12 @@ class DonationsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def donation_params
-    params.permit(:message_id, :timestamp, :type, :is_public,
+    ActionController::Parameters.new(JSON.parse(params[:data])).permit(
+      :message_id, :timestamp, :type, :is_public,
       :from_name, :message, :amount, :url, :email, :currency,
       :is_subscription_payment, :is_first_subscription_payment,
-      :kofi_transaction_id, :verification_token, :shop_items, :tier_name)
+      :kofi_transaction_id, :verification_token, :shop_items, :tier_name
+    )
   end
 
 end
