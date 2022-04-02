0d650149e9c332092065155d5a0f584f177441e5
Jascha Haldemann <jh@oxon.ch>
2022-04-02 23:55:47 +0200

    allows_emails_from_luxsmash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_211354) do
+ActiveRecord::Schema.define(version: 2022_04_02_235134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -460,6 +460,7 @@ ActiveRecord::Schema.define(version: 2022_04_02_211354) do
     t.boolean "allows_emails_from_germanysmash", default: true
     t.string "country_code"
     t.boolean "allows_emails_from_francesmash", default: true
+    t.boolean "allows_emails_from_luxsmash", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
