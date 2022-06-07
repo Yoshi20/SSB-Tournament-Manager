c437257b9bb366adddcd49687588febd98570599
Jascha Haldemann <jh@oxon.ch>
2022-06-07 18:26:25 +0200

    contact: impl. a honeypod for spam bots

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,14 +4,14 @@ class ContactController < ApplicationController
   def contact
     respond_to do |format|
       if verify_recaptcha(secret_key: ENV["RECAPTCHA_SECRET_KEY_#{session['country_code'].upcase}"], message: t('flash.alert.contact'))
-        if params[:email].present? && params[:body].present?
+        if params[:email].present? && params[:body].present? && !params[:url].present? # the :url is used to trick spam bots
           ContactMailer.with(name: params[:name], email: params[:email], body: params[:body], country_code: session['country_code']).contact_email.deliver_later
           format.html { redirect_to informations_path(anchor: 'contact'), notice: t('flash.notice.contact') }
         else
-          format.html { redirect_to informations_path(anchor: 'contact', name: params[:name], email: params[:email], body: params[:body] ), alert: t('flash.alert.contact_invalid') }
+          format.html { redirect_to informations_path(anchor: 'contact', name: params[:name], email: params[:email], body: params[:body], url: params[:url]), alert: t('flash.alert.contact_invalid') }
         end
       else
-        format.html { redirect_to informations_path(anchor: 'contact', name: params[:name], email: params[:email], body: params[:body] ) }
+        format.html { redirect_to informations_path(anchor: 'contact', name: params[:name], email: params[:email], body: params[:body], url: params[:url]) }
       end
     end
   end
