3a05602965a3506a40bd7b99b8fcb48979e30ac9
Jascha Haldemann <jh@oxon.ch>
2023-01-21 12:50:15 +0100

    show non public donations on welcome for super admin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,11 @@ class WelcomeController < ApplicationController
   # GET /welcome
   # GET /welcome.json
   def index
-    @donations = Donation.all_from(session['country_code']).where(is_public: true).order(Arel.sql('amount::float DESC')).limit(6)
+    if current_user.present? && current_user.super_admin?
+      @donations = Donation.all_from(session['country_code']).order(Arel.sql('amount::float DESC')).limit(6)
+    else
+      @donations = Donation.all_from(session['country_code']).where(is_public: true).order(Arel.sql('amount::float DESC')).limit(6)
+    end
     render "index_#{session['country_code']}"
   end
 
