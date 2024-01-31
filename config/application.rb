573be2be61ac7f24cbfea4e487f0ecce8781759e
Jascha Haldemann <jh@oxon.ch>
2024-01-31 15:40:48 +0100

    Revert "bin/rails app:update"
    This reverts commit 1c781966171d6cb6a68a2f844af92476e29a8ac5.

--- config/application.rb
+++ config/application.rb
@@ -1,28 +1,19 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SsbTournamentManager
+module SSBTournamentManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
     config.middleware.insert_before ActionDispatch::Static, Rack::Deflater # to compress (gzip) static assets
 
