6ca1a18d86c4f76329cdf98c31b9ea6abb04fcf6
Jascha Haldemann <jh@oxon.ch>
2024-07-23 18:41:32 +0200

    filter_by & sort_by

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -4,8 +4,18 @@ class ShopController < ApplicationController
 
   # GET /shop or /shop.json
   def index
-    @shop_products = Shop::Product.all.includes(:user, user: :player).order(:user_id, :position, :created_at)
+    @shop_products = Shop::Product.all.includes(:user, user: :player)
     @shopping_cart = Shop::ShoppingCart.find_latest(request.remote_ip, current_user&.id, session['session_id'])
+    # handle filter parameter
+    @shop_products = @shop_products.where(user_id: params[:seller]) if params[:seller].present?
+    @shop_products = @shop_products.where(subtype: params[:subtype]) if params[:subtype].present?
+    # handle order parameter
+    if params[:order].present?
+      sanitizedOrder = ActiveRecord::Base.sanitize_sql_for_order(params[:order].gsub('-', ' '))
+      @shop_products = @shop_products.order(Arel.sql(sanitizedOrder))
+    end
+    # in any case order by user_id & position at the end
+    @shop_products = @shop_products.order(:user_id, :position)
   end
 
 end
