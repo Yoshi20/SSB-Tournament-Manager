17baf32827b41697dffed0423dda17f189fb160d
Jascha Haldemann <jh@oxon.ch>
2025-02-02 13:27:52 +0100

    contact: prevent certain spam

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,7 +5,10 @@ class ContactController < ApplicationController
     respond_to do |format|
       if verify_recaptcha(secret_key: ENV["RECAPTCHA_SECRET_KEY_#{session['country_code'].upcase}"], message: t('flash.alert.contact'))
         if params[:email].present? && params[:body].present? && !params[:url].present? # the :url is used to trick spam bots
-          ContactMailer.with(name: params[:name], email: params[:email], body: params[:body], country_code: session['country_code']).contact_email.deliver_later
+          # also do net send anything when the message contains certain keywords
+          unless params[:body].include?('leadgeneration.com')
+            ContactMailer.with(name: params[:name], email: params[:email], body: params[:body], country_code: session['country_code']).contact_email.deliver_later
+          end
           format.html { redirect_to informations_path(anchor: 'contact'), notice: t('flash.notice.contact') }
         else
           format.html { redirect_to informations_path(anchor: 'contact', name: params[:name], email: params[:email], body: params[:body], url: params[:url]), alert: t('flash.alert.contact_invalid') }
